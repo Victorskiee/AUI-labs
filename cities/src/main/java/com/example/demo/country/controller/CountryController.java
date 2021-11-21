352cdf976909f0a4f73aaaac9f21f9240c068dcc
Victorskiee <krasinski.wiktor00@gmail.com>
2021-11-21 22:23:26 +0100

    cities app -> added POST and DELETE in country controller

--- cities/src/main/java/com/example/demo/country/controller/CountryController.java
+++ cities/src/main/java/com/example/demo/country/controller/CountryController.java
@@ -1,163 +1,49 @@
 package com.example.demo.country.controller;
 
-import com.example.demo.city.dto.*;
-import com.example.demo.city.entity.City;
-import com.example.demo.city.service.CityService;
+import com.example.demo.country.dto.CreateCountryRequest;
 import com.example.demo.country.entity.Country;
 import com.example.demo.country.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("api/countries")
 public class CountryController {
 
-    private CityService cityService;
-
     private CountryService countryService;
 
     @Autowired
-    public CountryController(CityService cityService, CountryService countryService){
-        this.cityService = cityService;
+    public CountryController(CountryService countryService){
         this.countryService = countryService;
     }
 
-    @GetMapping("{country}/cities")
-    public ResponseEntity<GetCitiesResponse> getCities(@PathVariable("country") String name){
-        Optional<Country> country = countryService.find(name);
-        if(country.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-        else{
-            List<City> cities = cityService.findAll(country.get());
-            GetCitiesResponse response = GetCitiesResponse.entityToDtoMapper(cities);
-            return ResponseEntity.ok(response);
-        }
-    }
-
-    @GetMapping("{country}/cities/{id}")
-    public ResponseEntity<GetCityResponse> getCity(@PathVariable("country") String name, @PathVariable("id") int id){
-        Optional<City> city = cityService.find(name, id);
-        if(city.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-        else{
-            GetCityResponse response = GetCityResponse.entityToDtoMapper(city.get());
-            return ResponseEntity.ok(response);
-        }
-    }
-
-    @PostMapping("{country}/cities")
-    public ResponseEntity<Void> createCity(@PathVariable("country") String name,
-                                           @RequestBody CreateCityRequest request,
-                                           UriComponentsBuilder builder){
-        Optional<Country> country = countryService.find(name);
-        if(country.isPresent()){
-            City city = CreateCityRequest.dtoToEntityMapper(request, country.get());
-            city = cityService.create(city);
-            return ResponseEntity.created(builder.pathSegment("api", "countries", "{country}", "cities")
-                    .buildAndExpand(country.get().getName(), city.getId()).toUri()).build();
-        }
-        else{
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @DeleteMapping("{country}/cities/{id}")
-    public ResponseEntity<Void> deleteCity(@PathVariable("country") String name,
-                                           @PathVariable("id") int id){
-        Optional<City> city = cityService.find(name, id);
-        if(city.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-        else{
-            cityService.delete(city.get().getId());
-            return ResponseEntity.accepted().build();
-        }
-    }
-
-    @PutMapping("{country}/cities/{id}")
-    public ResponseEntity<Void> updateCity(@PathVariable("country") String name,
-                                           @PathVariable("id") int id,
-                                           @RequestBody UpdateCityRequest request){
-        Optional<City> city = cityService.find(name, id);
-        if(city.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-        else{
-            City c = city.get();;
-            UpdateCityRequest.dtoToEntityUpdater(c, request);
-            cityService.update(c);
-            return ResponseEntity.accepted().build();
-        }
-    }
-
-    //----------------------------------------------------------------------------------
-
-    /*@GetMapping
-    public ResponseEntity<GetCountriesResponse> getCountries(){
-        List<Country> all = countryService.findAll();
-        GetCountriesResponse response = GetCountriesResponse.entityToDtoMapper(all);
-        return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("{country}")
-    public ResponseEntity<GetCountryResponse> getCountry(@PathVariable("country") String name){
-        Optional<Country> country = countryService.find(name);
-        if(country.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-        else{
-            GetCountryResponse response = GetCountryResponse.entityToDtoMapper(country.get());
-            return ResponseEntity.ok(response);
-        }
-    }
-
-    @PostMapping
-    public ResponseEntity<Void> createCountry(@RequestBody CreateCountryRequest request,
-                                              UriComponentsBuilder builder){
-        Optional<Country> c = countryService.find(request.getName());
-        if(c.isEmpty()) {
-            Country country = CreateCountryRequest.dtoToEntityMapper(request);
-            countryService.create(country);
-
-            return ResponseEntity.created(builder.pathSegment("api", "countries", "{country}")
-                    .buildAndExpand(country.getName()).toUri()).build();
-        }
-        else{
-            return ResponseEntity.badRequest().build();
-        }
+    @PostMapping("")
+    public ResponseEntity<Void> createCountry(@RequestBody CreateCountryRequest request, UriComponentsBuilder builder){
+       Country country = CreateCountryRequest.dtoToEntityMapper(request);
+       countryService.create(country);
+       return ResponseEntity
+               .created(builder
+                        .pathSegment("api", "countries", "{country}")
+                        .buildAndExpand(country.getName()).toUri())
+               .build();
     }
 
     @DeleteMapping("{country}")
     public ResponseEntity<Void> deleteCountry(@PathVariable("country") String name){
         Optional<Country> country = countryService.find(name);
-        if(country.isEmpty()){
-            return ResponseEntity.notFound().build();
+        if (country.isPresent()){
+            countryService.delete(country.get().getName());
+            return ResponseEntity
+                    .accepted()
+                    .build();
         }
         else{
-            countryService.delete(country.get().getName());
-            return ResponseEntity.accepted().build();
+            return ResponseEntity
+                    .notFound()
+                    .build();
         }
     }
-
-    @PutMapping("{country}")
-    public ResponseEntity<Void> updateCountry(@PathVariable("country") String name,
-                                           @RequestBody UpdateCountryRequest request){
-        Optional<Country> country = countryService.find(name);
-        if(country.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-        else{
-            Country c = country.get();
-            UpdateCountryRequest.dtoToEntityUpdater(c, request);
-            countryService.update(c);
-            return ResponseEntity.accepted().build();
-        }
-    }*/
 }
