53db1fc77d5ee4762be7fca079c2f5873f7a3c7b
Victorskiee <krasinski.wiktor00@gmail.com>
2021-11-23 21:50:12 +0100

    js app -> added javascript file handling countries

---
+++ js/list_of_countries/list_of_countries.js
@@ -0,0 +1,52 @@
+import {clearElementChildren, createLinkCell, createTextCell, createButtonCell} from "../js_scripts/utilities";
+import {getBackendUrl} from "../js_scripts/configuration";
+
+window.addEventListener('load', () => {
+    fetchAndDisplayCountries();
+});
+
+function fetchAndDisplayCountries() {
+   const xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function (){
+       if (this.readyState === 4 && this.status === 200){
+           displayCountries(JSON.parse(this.responseText));
+       }
+   };
+
+   xhttp.open('GET', getBackendUrl() + '/api/countries', true);
+   xhttp.send();
+}
+
+/**
+ * Updates the DOM tree in order to display users.
+ *
+ * @param {{countries: string[]}} countries
+ */
+function displayCountries(countries) {
+    let tableBody = document.getElementById('tableBody');
+    clearElementChildren(tableBody);
+    countries.countries.forEach(country => tableBody.appendChild(createTableRow(country)));
+}
+
+function createTableRow(country) {
+   let tr = document.createElement("tr");
+   const tdText = createTextCell(country);
+   const tdLink = createLinkCell('view', '../view_of_country/view_of_country.html?country=' + country);
+   const tdDelete = createButtonCell('delete', () => deleteCountry(country));
+   tr.appendChild(tdText);
+   tr.appendChild(tdLink);
+   tr.appendChild(tdDelete);
+   return tr;
+}
+
+function deleteCountry(country) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function (){
+        if (this.readyState === 4 && this.status === 202){
+            fetchAndDisplayCountries();
+        }
+    };
+    
+    xhttp.open("DELETE", getBackendUrl() + '/api/countries' + country, true);
+    xhttp.send();
+}
