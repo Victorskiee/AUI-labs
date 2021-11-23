676cb0b7499d345f75526f8a73e99fbbe468062d
Victorskiee <krasinski.wiktor00@gmail.com>
2021-11-24 00:47:29 +0100

    js app -> accepted from js branch

---
+++ js/edit_of_city/edit_of_city.js
@@ -0,0 +1,51 @@
+import {getParameterByName} from "../js_scripts/utilities.js";
+import {getBackendUrl} from "../js_scripts/configuration.js";
+
+window.addEventListener('load', () => {
+    const infoForm = document.getElementById('infoForm');
+
+    infoForm.addEventListener('submit', event => updateInfoAction(event));
+
+    fetchAndDisplayCity();
+});
+
+function fetchAndDisplayCity(){
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function (){
+        if (this.readyState === 4 && this.status === 200){
+            let response = JSON.parse(this.responseText);
+            for (const [key, value] of Object.entries(response)){
+                let input = document.getElementById(key);
+                if (input){
+                    input.value = value;
+                }
+            }
+        }
+    };
+
+    xhttp.open("GET", getBackendUrl() + '/api/countries/' + getParameterByName('country')
+        + '/cities/' + getParameterByName('city'), true);
+    xhttp.send();
+}
+
+function updateInfoAction(event){
+    event.preventDefault();
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function (){
+        if (this.readyState === 4 && this.status === 201){
+            fetchAndDisplayCity();
+        }
+    };
+
+    xhttp.open("PUT", getBackendUrl() + '/api/countries/' + getParameterByName('country')
+        + '/cities/' + getParameterByName('city'), true);
+
+    const request = {
+        'name': document.getElementById('name').value,
+        'nrOfStreets': document.getElementById('nrOfStreets').value
+    };
+
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.send(JSON.stringify(request));
+}
